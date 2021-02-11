a9047a23e7aca582aca28fadbdcbe17e249dd77e
Cameron Howard <camhoward93@gmail.com>
2021-02-10 17:18:26 -0800

    Add files via upload

---
+++ CreateDB.py
@@ -0,0 +1,28 @@
+# NOTE: The reading / writing of this can be parallelized
+
+# Imports
+import pandas as pd
+import csv
+
+# Global Column Values
+
+# Create the header row for output csv
+# possibly declare all desired variables to make the looping easier?
+# or store it in a data structure to pass to a function. ez pz lemon squeezy
+
+# Try to open csv
+
+# If successful, enter header row of csv
+
+# Try to open the databse php
+
+# If successful, take heading up to first card
+
+# While not at the end of the file
+    # Take in a card
+
+    # Parse the desired values
+
+    # Enter the card into the csv
+
+# Close both files and reap the benefits of this brand new database
