f1f27c1a721fd9ae90f3f922a8ebc6a233b0824c
Cameron Howard <camhoward93@gmail.com>
2021-02-12 21:25:51 -0800

    Needs a bit more work
    Updated work in progress. Some of the entries in the output csv are getting separated strangely.

--- CreateDB.py
+++ CreateDB.py
@@ -1,28 +1,55 @@
-# NOTE: The reading / writing of this can be parallelized
+# NOTE: The output name still has backslashes on the special characters from the php file
+#       Ultimately this doesn't matter since we can compare by ID, but it can be cleaned up later if desired
+#       The runtime is also pretty slow with larger databases. The code can be parallelized if needed
 
 # Imports
-import pandas as pd
 import csv
+import re
 
-# Global Column Values
+# Functions
+def create_header(attributes, outfile):
+    # Fill the top line of the outfile with the attributes
+    with open(outfile, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(attributes)
 
-# Create the header row for output csv
-# possibly declare all desired variables to make the looping easier?
-# or store it in a data structure to pass to a function. ez pz lemon squeezy
+def download_cards(infile, regex):
+    # declare result list
+    cardList = list()
 
-# Try to open csv
+    # DEBUG_COUNT = 0
 
-# If successful, enter header row of csv
+    # for each line in the input file (NOTE: I think our input php is one long line)
+    for line in open(infile):
+        # for each card found
+        for match in re.finditer(regex, line):
+            # add fields to card list
+            print(match.group(1))
+            cardList.append([match.group(1), match.group(2), match.group(3)])
+            # if(DEBUG_COUNT > 5):
+            #     break
+            # ++DEBUG_COUNT
 
-# Try to open the databse php
+    # return the card list
+    return cardList
 
-# If successful, take heading up to first card
+def upload_cards(cardList, outfile):
+    # Fill the csv with the card list
+    with open(outfile, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+        for card in cardList:
+            writer.writerow(card)
 
-# While not at the end of the file
-    # Take in a card
+# Declare variables
+outputFile = 'database.csv'
+inputFile = 'cardDatabase2-9.php'
+header = ['Card ID', 'Card Name', 'Card Type'] # more can be added based on needs
+pattern = re.compile(r'(?=\{\"id\":(\d+),\"name\":\"(.+?)\",\"type\":\"(.+?)\".*\},)')
 
-    # Parse the desired values
+# Create header row of csv
+create_header(header, outputFile)
+# Load the cards from the php
+cards = download_cards(inputFile, pattern)
+# Fill the csv with the card list
+upload_cards(cards, outputFile)
 
-    # Enter the card into the csv
-
-# Close both files and reap the benefits of this brand new database
