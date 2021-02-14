06d7e3e8e862db98c49152c2f7ce576f8f9f55ee
James Ludwig <Vongolajames@Jamess-MacBook-Pro-3.local>
2021-02-13 23:06:13 -0800

    updating Main.py to handle extra deck cards

--- Main.py
+++ Main.py
@@ -4,10 +4,15 @@
 # Imports
 import pandas as pd
 
-# Get columns
+# Get columns from collection
 df = pd.read_csv('collection.csv')
 quantity = df['cardq']
 ids = df['cardid']
+
+# Get extra deck ids
+df = pd.read_csv('database.csv')
+extra_ids = df[df['Card Type'].isin(['Fusion Monster', 'Synchro Monster', 'Synchro Tuner Monster', 'Synchro Pendulum Effect Monster', 'XYZ Monster', 'XYZ Pendulum Effect Monster', 'Link Monster', 'Pendulum Effect Fusion Monster'])]['Card ID'].values
+extra_list = []
 
 # Open the file and enter the main header
 f = open('decklist.ydk', 'w+')
@@ -15,12 +20,22 @@ f.write('#main\n')
 
 # Loop over the columns
 for q, id in zip(quantity, ids):
-    # Print id to file q times
-    for x in range(q):
-        f.write(str(id) + '\n')
+    # Check if card is extra deck card
+    if id in extra_ids:
+        #if so add to extra deck list q times
+        for x in range(q):
+            extra_list.append(id)
+        continue
+    else:
+        # Print id to file q times
+        for x in range(q):
+            f.write(str(id) + '\n')
 
 # Add extra and side headers
 f.write('\n#extra\n')
+for id in extra_list:
+    f.write(str(id) + '\n')
+
 f.write('\n!side\n')
 
 # Close the file
